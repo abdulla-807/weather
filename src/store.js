8fadc8916e4549e8cb5f8dd94b1720208decef53
Abdulla <abd@abd.com>
2023-09-05 19:07:57 +0500

    changed http

--- src/store.js
+++ src/store.js
@@ -20,7 +20,7 @@ const store = createStore({
   actions: {
     async setWeather({commit, state}, city){
       try {
-        let response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${state.apiKey}`);
+        let response = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${state.apiKey}`);
         let cityInfo = response.data[0]
         let {lat, lon, local_names } = cityInfo;
         let result = await axios.get(`https://api.openweathermap.org/data/2.8/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${state.apiKey}&units=metric&lang=ru`)
